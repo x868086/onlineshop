b035e7545c4ca557d3f08e9a3c78794dd7cd9895
x8086 <43070810@qq.com>
2018-08-28 00:08:41 +0800

    cartlist

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import router from './router'
+
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+
+import lazyload from 'vue-lazy-load';
+Vue.use(lazyload,{
+    loading: '../static/loading/loading-bars.svg',
+    try:3
+    // error: '加载错误图片'
+    }
+)
+
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
 })
---
+++ src/views/GoodsList.vue
@@ -0,0 +1,110 @@
+<template>
+    <div>
+        <nav-header></nav-header>
+        <nav-bread>
+            <span slot="bread">Goods</span>
+        </nav-bread>
+        <div class="accessory-result-page accessory-page">
+            <div class="container">
+                <div class="filter-nav">
+                <span class="sortby">Sort by:</span>
+                <a href="javascript:void(0)" class="default cur">Default</a>
+                <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+                <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+                </div>
+                <div class="accessory-result">
+                <!-- filter -->
+                <div class="filter stopPop" id="filter">
+                    <dl class="filter-price">
+                    <dt>Price:</dt>
+                    <dd v-for="(list,index) in pricebetween"
+                    v-bind:key="index">
+                    <a href="javascript:void(0)"
+                    v-on:click="addcur(index)"
+                    v-bind:class="{cur:index===curidx}">{{list}}</a>
+                    </dd>
+                    <!-- <dd>
+                        <a href="javascript:void(0)">0 - 100</a>
+                    </dd> -->
+                    </dl>
+                </div>
+
+                <!-- search result accessories list -->
+                <div class="accessory-list-wrap">
+                    <div class="accessory-list col-4">
+                    <ul>
+                        <li v-for="(img,index) in goodlist"
+                        v-bind:key="index">
+                        <div class="pic">
+                            <a href="#"><img v-bind:src="img.productImage" alt=""></a>
+                            <!-- <a href="#"><img v-lazy="'../../static/1.jpg'" alt=""></a> -->
+                        </div>
+                        <div class="main">
+                            <div class="name">{{img.productName}}</div>
+                            <div class="price">{{img.productPrice}}</div>
+                            <div class="btn-area">
+                            <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                            </div>
+                        </div>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        <nav-footer></nav-footer>
+    </div>
+</template>
+
+<script>
+import '../assets/css/base.css';
+import '../assets/css/product.css';
+import '../assets/css/login.css';
+import axios from 'axios';
+
+
+
+import NavHeader from '../components/NavHeader.vue';
+import NavFooter from '../components/NavFooter.vue';
+import NavBread from '../components/NavBread.vue';
+
+
+
+export default {
+    data(){
+        return {
+            goodlist:null,
+            pricebetween:null,
+            curidx:0
+        }
+    },
+
+    created:function(){
+        this.getGoodsList()
+    },
+
+    methods:{
+        getGoodsList(){
+        axios.get("http://rap2api.taobao.org/app/mock/83412/goodList").then(res=>{
+            this.goodlist=res.data.productList;
+            this.pricebetween=res.data.priceBetween;
+        })
+        },
+        addcur(index){
+            this.curidx=index
+        }
+    },
+
+    components:{
+        NavHeader,
+        NavFooter,
+        NavBread
+    }
+}
+</script>
+
+
+
+
+
